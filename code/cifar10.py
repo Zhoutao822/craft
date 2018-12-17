41ddd50b8441b1ef4d0ed065fdaa730cc0afdca4
zhuantou233 <228646830@qq.com>
2018-12-17 17:41:18 +0800

    update

--- code/cifar10.py
+++ code/cifar10.py
@@ -1,27 +1,96 @@
 # Copyright
 # License
 # ==============================================================================
-"""代码功能描述
-代码功能细节
+"""生成CIFAR10 Dataset
+版本：
+    TensorFlow：1.12
+    Python：3.6.7
+读取tfrecords文件，对图片和标签的数据类型进行调整，对图片进行扰乱处理，比如裁剪、
+亮度调整、对比度调整和翻转等操作，shuffle和batch，make_batch返回一个batch的数据，
+此部分代码改动较少。
 """
-
-#%%
-from __future__ import division
-from __future__ import absolute_import
-from __future__ import print_function
-
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras import layers
-from tensorflow.keras import Sequential 
+import os
 
 import tensorflow as tf
-import pandas as pd
-import numpy as np
-import os
-import shutil
 
-print('Tensorflow version: ', tf.VERSION)
+HEIGHT = 32
+WIDTH = 32
+DEPTH = 3
 
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
-print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
+class Cifar10DataSet(object):
+    """通过一个类来管理dataset"""
+    def __init__(self, data_dir, subset='train', use_distortion=True):
+        self.data_dir = data_dir
+        self.subset = subset
+        self.use_distortion = use_distortion
+
+    def get_filenames(self):
+        if self.subset in ['train', 'validation', 'eval']:
+            return [os.path.join(self.data_dir, self.subset + '.tfrecords')]
+        else:
+            raise ValueError('Invalid data subset {}'.format(self.subset))
+
+    def parser(self, example):
+        """读取tfrecords文件，类型转换，shape调整"""
+        features = tf.parse_single_example(
+            example, 
+            features={
+                'image': tf.FixedLenFeature([], tf.string),
+                'label': tf.FixedLenFeature([], tf.int64)
+            })
+        image = tf.decode_raw(features['image'], tf.uint8)
+        image.set_shape([DEPTH * HEIGHT * WIDTH])
+
+        image = tf.cast(
+            tf.transpose(tf.reshape(image, [DEPTH, HEIGHT, WIDTH]), [1, 2, 0]),
+            tf.float32)
+        label = tf.cast(features['label'], tf.int32)
+
+        image = self.preprocess(image)
+        return image, label
+
+    def preprocess(self, image):
+        """对train数据集进行扰乱，包括裁剪、亮度调整、对比度调整和翻转等操作"""
+        if self.subset == 'train' and self.use_distortion:
+            image = tf.image.resize_image_with_crop_or_pad(image, 40, 40)
+            image = tf.random_crop(image, [HEIGHT, WIDTH, DEPTH]) # 裁剪
+            image = tf.image.random_flip_left_right(image) # 左右翻转
+            # image = tf.image.random_brightness(image, max_delta=10) # 亮度
+            # image = tf.image.random_contrast(image, lower=0.2, upper=1.8) # 对比度
+            # image = tf.image.random_hue(image, max_delta=0.1) # 色相
+            # image = tf.image.random_flip_up_down(image) # 上下翻转
+            # image = tf.image.random_saturation(image, 0, 5) # 饱和度
+            # image = tf.image.random_jpeg_quality(image, 50, 90) # 噪声，jpeg质量
+        return image
+
+    def make_batch(self, batch_size):
+        """通过TFRecordDataset读取文件，shuffle和batch数据集，返回一个batch的数据"""
+        filenames = self.get_filenames()
+        dataset = tf.data.TFRecordDataset(filenames).repeat()
+        # num_parallel_calls并行处理，加速IO
+        dataset = dataset.map(
+            self.parser, num_parallel_calls=batch_size)
+        # 缓冲池的大小设计
+        if self.subset == 'train':
+            min_queue_examples = int(
+                Cifar10DataSet.num_examples_per_epoch(self.subset) * 0.4)
+            dataset = dataset.shuffle(buffer_size=min_queue_examples + 3 * batch_size)
+
+        dataset = dataset.batch(batch_size)
+        iterator = dataset.make_one_shot_iterator()
+        image_batch, label_batch = iterator.get_next()
+
+        return image_batch, label_batch
+
+    @staticmethod
+    def num_examples_per_epoch(subset='train'):
+        if subset == 'train':
+            return 40000 # 对源码进行了修改
+        elif subset == 'validation':
+            return 10000
+        elif subset == 'eval':
+            return 10000
+        else:
+            raise ValueError('Invalid data subset "%s"' % subset)
+    
--- code/generate_cifar10_tfrecords.py
+++ code/generate_cifar10_tfrecords.py
@@ -1,22 +1,27 @@
 # Copyright
 # License
 # ==============================================================================
-"""代码功能描述
-代码功能细节
+"""根据cifar10数据集生成TFRecords文件
+版本：
+    TensorFlow：1.12
+    Python：3.6.7
+使用keras.datasets.cifar10.load_data()获得数据，训练集中划分后20%作为验证集，
+通过TFRecordWriter写入到三个文件中：train.tfrecords, validation.tfrecords, 
+eval.tfrecords，运行时参数data_dir指定生成文件的路径。
 """
-#coing=utf-8
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
 import os
 
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string(
-    'data_dir', './cifar10', 'Directory to download and extract CIFAR-10 to.')
+    'data_dir', './cifar10', 'Directory to generate tfrecords to.')
 FILE_NAMES = ['train', 'validation', 'eval']
 
 
@@ -35,16 +40,31 @@ def convert_to_tfrecord(x, y, output_file):
         for i in range(data_length):
             example = tf.train.Example(features=tf.train.Features(
                 feature={
+                    # 通过keras获得的数据集的image是uint8类型的数据
                     'image': _bytes_feature(x[i].tobytes()),
-                    'label': _int64_feature(y[i])
+                    # 通过keras获得的数据集的label是[xxx, 1]的形状，类型int32，
+                    # 需要y[i, 0]获得标签数值，类型转换为int64，
+                    # tfrecords只支持Int64List，没有Int32List
+                    'label': _int64_feature(y[i, 0].astype(np.int64))
                 }))
             writer.write(example.SerializeToString())
     print('Generate {} success!'.format(output_file))
 
 
 def main(data_dir):
+    """
+    参数：
+        data_dir：tfrecords文件保存路径
+    功能：
+        主函数，包括生成文件夹，获取数据，划分数据，生成tfrecords文件
+    """
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
     print('Start to generate tfrecords in {}.'.format(data_dir))
+    # 调用keras.datasets.cifar10.load_data()获得数据
     (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+    # 这里划分前80%的数据做训练集，20%验证集，理论上要shuffle，
+    # 这里我感觉不shuffle也行
     split_index = int(len(y_train) * 0.8)
     assert len(x_train) == len(y_train)
     val_data = x_train[split_index:], y_train[split_index:]
@@ -60,6 +80,6 @@ def main(data_dir):
         convert_to_tfrecord(x, y, output_file)
     print('Done!')
 
-
 if __name__ == '__main__':
+    # 通过tensorflow的flags产生运行时参数，简单一些
     main(FLAGS.data_dir)

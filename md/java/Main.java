1f8aaa3477febe4d9f6dec238565bdf9add0744c
zhuantou233 <228646830@qq.com>
2019-03-25 17:52:48 +0800

    update

--- md/java/Main.java
+++ md/java/Main.java
@@ -1,9 +1,15 @@
-import java.util.concurrent.atomic.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 class Main{
     public static void main(String[] args) {
-        AtomicInteger i = new AtomicInteger(0);
-        i.getAndIncrement();
-        System.out.print(i);
+        // AtomicInteger i = new AtomicInteger(0);
+        // i.getAndIncrement();
+        // System.out.print(i);
+        // int a = 1;
+        // int b = 1;
+        Integer a = new Integer(1);
+        Integer b = new Integer(1);
+        System.out.print(a == b);
+        ConcurrentHashMap map = new ConcurrentHashMap();
     }
 }
